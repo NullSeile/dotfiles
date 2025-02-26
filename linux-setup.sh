9588266f1628f5b79d019ad5c268583ada33474f
nullptr <lia@bertran.xyz>
2025-02-26 13:05:15 +0100

    Minor updates

--- linux-setup.sh
+++ linux-setup.sh
@@ -3,12 +3,21 @@
 cd "$(dirname $0)"
 dir=`pwd`
 
+
+
 function create_symlink() {
     if [ -e "$2" ]; then
-        echo "Removing $2"
+        echo "File '$2' already exists."
         rm -ri "$2"
     fi
     ln -s "$dir/$1" "$2"
+}
+function sudo_create_symlink() {
+    if [ -e "$2" ]; then
+        echo "File '$2' already exists."
+        sudo rm -ri "$2"
+    fi
+    sudo ln -s "$dir/$1" "$2"
 }
 
 create_symlink i3 ~/.config/regolith3/i3
@@ -17,8 +26,8 @@ create_symlink i3 ~/.config/i3
 create_symlink nvim ~/.config/nvim
 
 create_symlink kanata ~/.config/kanata
-create_symlink kanata/kanata.service ~/.config/systemd/user/kanata.service
-systemctl --user enable kanata
+sudo_create_symlink kanata/kanata.service /etc/systemd/system/kanata.service
+sudo systemctl enable kanata
 
 create_symlink wezterm ~/.config/wezterm
 create_symlink polybar ~/.config/polybar
